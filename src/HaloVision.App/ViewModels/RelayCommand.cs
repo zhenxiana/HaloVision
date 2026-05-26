67d9ec75e42f4084945824c23d59527c324b10fa
zhenxiana <92145826+zhenxiana@users.noreply.github.com>
2026-05-26 21:57:14 +0800

    Fix C# 10 compatibility in RelayCommand

--- src/HaloVision.App/ViewModels/RelayCommand.cs
+++ src/HaloVision.App/ViewModels/RelayCommand.cs
@@ -2,13 +2,22 @@ using System.Windows.Input;
 
 namespace HaloVision.App.ViewModels;
 
-public class RelayCommand(Action execute, Func<bool>? canExecute = null) : ICommand
+public class RelayCommand : ICommand
 {
+    private readonly Action _execute;
+    private readonly Func<bool>? _canExecute;
+
+    public RelayCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
     public event EventHandler? CanExecuteChanged;
 
-    public bool CanExecute(object? parameter) => canExecute?.Invoke() ?? true;
+    public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
 
-    public void Execute(object? parameter) => execute();
+    public void Execute(object? parameter) => _execute();
 
     public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
 }
